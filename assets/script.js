6d9ff79df91c91cb5bdc1f2d787a732df06497f5
Stefano <stefano.turcarelli@icloud.com>
2023-11-06 21:51:50 -0600

    Fix logic preventing to win at attempt 1

--- assets/script.js
+++ assets/script.js
@@ -73,39 +73,41 @@ const guessBtn = select('#guess-button');
 const instructions = select('.instructions');
 let count = 5;
 let randomNum = randomNumber(1, 10);
-
-// print(`Random Number: ${randomNum}`);
+print(`Random Number: ${randomNum}`);
 
 function validateInput(number) {
   const inputNum = parseInt(number.trim());
   if (isNaN(inputNum)) {
-    return (output.innerText = `Please enter a number`);
+    output.innerText = `Please enter a number`;
+  } else if (count === 1) {
+    count--;
+    output.innerText = `Out of attempts! The secret number was ${randomNum}`;
+    guessBtn.style.display = 'none';
+    resetBtn.style.display = 'block';
   }
   return inputNum;
 }
 
 function getHint(number) {
-  const inputNum = parseInt(number.trim());
-  if (count > 1) {
-    if (inputNum > randomNum) {
-      output.innerText = `Try a lower number`;
-    } else if (inputNum < randomNum) {
-      output.innerText = `Try a higher number`;
-    } else if (inputNum > 10) {
-      output.innerText = `Please enter a number between 1 and 10`;
-    } else if (inputNum === randomNum) {
-      output.innerText = `Correct! The secret number was: ${randomNum}`;
-      resetBtn.style.display = 'block';
-    }
-  } else {
-    output.innerText = `Out of attempts! The secret number was ${randomNum}`;
-    guessBtn.style.display = 'none';
+  if (number > randomNum && count > 1) {
+    count--;
+    console.log(`Count: ${count}`);
+    output.innerText = `Try a lower number`;
+  } else if (number < randomNum && count > 1) {
+    count--;
+    console.log(`Count: ${count}`);
+    output.innerText = `Try a higher number`;
+  } else if (number > 10 && count > 1) {
+    output.innerText = `Please enter a number lower than 10`;
+  } else if (number === randomNum && count >= 0) {
+    output.innerText = `Correct! The secret number was: ${randomNum}`;
     resetBtn.style.display = 'block';
   }
 }
 
 function resetGame() {
   randomNum = randomNumber(1, 10);
+  print(`Random Number: ${randomNum}`);
   input.value = '';
   guessBtn.style.display = 'block';
   output.innerText = `Please enter a number between 1 and 10`;
@@ -119,18 +121,15 @@ onEvent('load', window, () => {
   output.style.display = 'none';
   input.value = '';
   guessQty.innerText = `${count}`;
-  // print(`Count: ${count}`);
 });
 
 onEvent('click', guessBtn, () => {
+  let guessNumber = input.value;
+  let validatedInput = validateInput(guessNumber);
+  getHint(validatedInput);
   instructions.style.display = 'none';
   output.style.display = 'block';
-  let guessNumber = input.value;
-  validateInput(guessNumber);
-  getHint(guessNumber);
-  count--;
   guessQty.innerText = `${count}`;
-  // print(`Count: ${count}`);
 });
 
 onEvent('click', resetBtn, () => {
